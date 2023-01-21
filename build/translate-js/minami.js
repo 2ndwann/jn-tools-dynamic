15784551a7a2da8dd798179a138587a7039bba54
2ndwann <guildwood.a@yahoo.com>
2023-01-20 19:34:12 -0500

    commit

---
+++ build/translate-js/minami.js
@@ -0,0 +1,88 @@
+export function minamiTranslate(s) {
+	s = s.replace(/あ/g, '安');
+	s = s.replace(/い/g, '以');
+	s = s.replace(/う/g, '宇');
+	s = s.replace(/え/g, '衣');
+	s = s.replace(/お/g, '於');
+
+	s = s.replace(/か/g, '加');
+	s = s.replace(/き/g, '機');
+	s = s.replace(/く/g, '久');
+	s = s.replace(/け/g, '計');
+	s = s.replace(/こ/g, '己');
+
+	s = s.replace(/さ/g, '左');
+	s = s.replace(/し/g, '之');
+	s = s.replace(/す/g, '寸');
+	s = s.replace(/せ/g, '世');
+	s = s.replace(/そ/g, '曽');
+
+	s = s.replace(/た/g, '太');
+	s = s.replace(/ち/g, '知');
+	s = s.replace(/つ/g, '川');
+	s = s.replace(/て/g, '天');
+	s = s.replace(/と/g, '止');
+
+	s = s.replace(/な/g, '奈');
+	s = s.replace(/に/g, '仁');
+	s = s.replace(/ぬ/g, '奴');
+	s = s.replace(/ね/g, '袮');
+	s = s.replace(/の/g, '乃');
+
+	s = s.replace(/は/g, '波');
+	s = s.replace(/ひ/g, '比');
+	s = s.replace(/ふ/g, '不');
+	s = s.replace(/へ/g, '部');
+	s = s.replace(/ほ/g, '保');
+
+	s = s.replace(/ま/g, '未');
+	s = s.replace(/み/g, '美');
+	s = s.replace(/む/g, '武');
+	s = s.replace(/め/g, '女');
+	s = s.replace(/も/g, '毛');
+
+	s = s.replace(/や/g, '也');
+	s = s.replace(/ゆ/g, '由');
+	s = s.replace(/よ/g, '与');
+
+	s = s.replace(/ら/g, '良');
+	s = s.replace(/り/g, '利');
+	s = s.replace(/る/g, '留');
+	s = s.replace(/れ/g, '礼');
+	s = s.replace(/ろ/g, '呂');
+
+	s = s.replace(/わ/g, '和');
+	s = s.replace(/を/g, '遠');
+
+	s = s.replace(/ん/g, '无');
+	
+	s = s.replace(/が/g, '加');
+	s = s.replace(/ぎ/g, '機');
+	s = s.replace(/ぐ/g, '久');
+	s = s.replace(/げ/g, '計');
+	s = s.replace(/ご/g, '己');
+
+	s = s.replace(/ざ/g, '左');
+	s = s.replace(/じ/g, '之');
+	s = s.replace(/ず/g, '寸');
+	s = s.replace(/ぜ/g, '世');
+	s = s.replace(/ぞ/g, '曽');
+
+	s = s.replace(/だ/g, '太');
+	s = s.replace(/ぢ/g, '知');
+	s = s.replace(/づ/g, '川');
+	s = s.replace(/で/g, '天');
+	s = s.replace(/ど/g, '止');
+
+	s = s.replace(/ば/g, '波');
+	s = s.replace(/び/g, '比');
+	s = s.replace(/ぶ/g, '不');
+	s = s.replace(/べ/g, '部');
+	s = s.replace(/ぼ/g, '保');
+	
+	s = s.replace(/ぱ/g, '波');
+	s = s.replace(/ぴ/g, '比');
+	s = s.replace(/ぷ/g, '不');
+	s = s.replace(/ぺ/g, '部');
+	s = s.replace(/ぽ/g, '保');
+}
