b82f1dfb8deb0932fb55a1a40ede5ed996129f72
2ndwann <guildwood.a@yahoo.com>
2023-01-25 01:24:15 -0500

    commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.set('view engine', 'ejs');
+
+
+app.get("/", (req, res) => {
+	res.send('hi');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
