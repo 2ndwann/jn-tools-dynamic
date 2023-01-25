456be50b85de4044f1aace04008755d466eb01c8
2ndwann <guildwood.a@yahoo.com>
2023-01-25 04:16:06 -0500

    Added

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+	apiKey: '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',
+});
+const openai = new OpenAIApi(configuration);
+
 const express = require("express");
+const expressSanitizer = require('express-sanitizer');
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(expressSanitizer());
 
 app.get("/", (req, res) => {
 	res.render('index');
 });
+
+// Translate feature
 app.get("/translator", (req, res) => {
-	res.render('translator');
+	if (req.sanitize(req.query.inputText)) {
+		// Attempt to translate from Weskethian
+		async function getResponse () {
+			let response = await openai.createCompletion({
+				model: "text-davinci-003",
+				prompt: "Convert this text to a fantasy dialect of English, known as Weskethian Commonspeak.\n\nExample:\n“I’m a farmer.”\n“The mill is working.”\n“He told me the mill is running.”\n“Yes, I'm happy.”\n“That dog isn't clever!”\n“The mill wasn’t running yesterday.”\n\nOutput: \n“I’o’a farmer.”\n“The mill oyes working.”\n“He told me the mill oad running.”\n“Yes, i’o happy.”\n“That dog oyn’t clever!”\n“The mill oadn’t running yesterday.”\n\nAdditional vocabulary examples:\nRun (noun) ⇔ Small river,\nSageon (adjective, but can also be a noun) ⇔ Extremely old, but not ancient or prehistoric.\n      Example: “This wine is sageon!”\nSithe (noun)⇔ A distance of four chunks; 64 blocks.\nHalf (noun) ⇔ A distance of a little more than one block. \nHorse (noun) ⇔ A horse in Weskethian Commonspeak is slightly different than horses in English, as they mean all 4-legged animals that you can ride on. This means donkeys and mules are also “horses”. However horse may also refer to real horses if used in a sentence with donkeys and mules.\nSanking (noun) ⇔ A formal gathering, meeting or assembly (exception domestic council meeting such as Wesketh Council Meeting), where diplomacy between factions is discussed\nWyrcann (noun) ⇔ the construction, crafting, making, forging or production\nWyrn (verb) ⇔ To craft, make, forge, construct or produce.\nAiel/Aieles (proper noun) ⇔ The ones who lived in Norholt before the hobbits (the ildras)\nThe Gard (proper noun) ⇔ The entire world of Jehvnar. (Within the horizon of Wesketh) \nAacn (noun) ⇔ An oak\nMisril (noun) ⇔ Mithril\nQuarr (noun) ⇔ A mine or quarry.\nSigil (noun) ⇔ A very fine or valuable gem.\nGlorest (adjective) ⇔ Something old and immovable that is very prideful. (e.g. A mountain)\nFyrnes (adjective) ⇔ Extremely old, ancient and prehistoric. Something that has always oad.\nMirige (noun) ⇔ Someone sweet, kind or pleasant. \nWintered (noun) ⇔ Someone old, but still living. \nRufe (noun) ⇔ Red and golden\nFormous (adjective) ⇔  Beautiful, especially in a shining fasion.\nSmiking (adjective) ⇔ Beautiful. \nMoise (noun) ⇔ One of the three seasons in wesketh. Equivalent to spring and early/mid summer. \nMoin (idiom) ⇔ hey or good day\nOu ⇔ \ninformal: Can mean I, you (singular), he, she or it\nFormal: “the”, Ou Gard, Ou moisy mornin (Always capitalized)\nOum (pronoun) ⇔ Yours/Your/Its; possessive pronoun\nOume (pronoun) ⇔ Accusative and Dative of Ou. (me, her, him, us, them)\nGelba (noun) ⇔ yellow\nAgena (noun) ⇔ old\nThred (verb)  ⇔ to think. To thred alike means to be on the same page.\nPip (noun) ⇔ apple. Pip tree is an apple tree. One may say pips and pears.\nEn (conjunction) ⇔ and\t\nHoobit (idiom) ⇔ bless you\nMirigity (noun)  ⇔ The state of being Mirige\nGelfre (adj)  ⇔ Generous\nGelfrity (noun)  ⇔ Generosity\nRetta (noun)  ⇔ Letter\nRuthred (noun) ⇔ (Royal Game of Ur) \nAlternative : Thrufe (South Riding)\nArcoiean (adj) ⇔ Magic/Arcane\nd’(art) ⇔ (informal) The\n\n\"" + req.sanitize(req.query.inputText) +"\"",
+				temperature: 0,
+				max_tokens: 256,
+				top_p: 1,
+				frequency_penalty: 0.2,
+				presence_penalty: 0,
+			});
+			
+			let translatedText = response.data.choices[0].text; // The translated text
+			
+			res.render('translator', {translatedText: translatedText, inputText: req.sanitize(req.query.inputText)});
+		}		
+		getResponse();
+	} else {
+		res.render('translator');
+	}
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- views/translate-js/minami.js
+++ views/translate-js/minami.js
@@ -1,4 +1,6 @@
-export function minamiTranslate(s) {
+
+function minamiTranslate(s) {
+	// Transliterate
 	s = s.replace(/あ/g, '安');
 	s = s.replace(/い/g, '以');
 	s = s.replace(/う/g, '宇');
@@ -85,4 +87,93 @@ export function minamiTranslate(s) {
 	s = s.replace(/ぷ/g, '不');
 	s = s.replace(/ぺ/g, '部');
 	s = s.replace(/ぽ/g, '保');
+	
+	s = s.replace(/ア/g, '安');
+	s = s.replace(/イ/g, '以');
+	s = s.replace(/ウ/g, '宇');
+	s = s.replace(/エ/g, '衣');
+	s = s.replace(/オ/g, '於');
+
+	s = s.replace(/カ/g, '加');
+	s = s.replace(/キ/g, '機');
+	s = s.replace(/ク/g, '久');
+	s = s.replace(/ケ/g, '計');
+	s = s.replace(/コ/g, '己');
+
+	s = s.replace(/サ/g, '左');
+	s = s.replace(/シ/g, '之');
+	s = s.replace(/ス/g, '寸');
+	s = s.replace(/セ/g, '世');
+	s = s.replace(/ソ/g, '曽');
+
+	s = s.replace(/タ/g, '太');
+	s = s.replace(/チ/g, '知');
+	s = s.replace(/ツ/g, '川');
+	s = s.replace(/テ/g, '天');
+	s = s.replace(/ト/g, '止');
+
+	s = s.replace(/ナ/g, '奈');
+	s = s.replace(/ニ/g, '仁');
+	s = s.replace(/ヌ/g, '奴');
+	s = s.replace(/ネ/g, '袮');
+	s = s.replace(/ノ/g, '乃');
+
+	s = s.replace(/ハ/g, '波');
+	s = s.replace(/ヒ/g, '比');
+	s = s.replace(/フ/g, '不');
+	s = s.replace(/ヘ/g, '部');
+	s = s.replace(/ホ/g, '保');
+
+	s = s.replace(/マ/g, '未');
+	s = s.replace(/ミ/g, '美');
+	s = s.replace(/ム/g, '武');
+	s = s.replace(/メ/g, '女');
+	s = s.replace(/モ/g, '毛');
+
+	s = s.replace(/ヤ/g, '也');
+	s = s.replace(/ユ/g, '由');
+	s = s.replace(/ヨ/g, '与');
+
+	s = s.replace(/ラ/g, '良');
+	s = s.replace(/リ/g, '利');
+	s = s.replace(/ル/g, '留');
+	s = s.replace(/レ/g, '礼');
+	s = s.replace(/ロ/g, '呂');
+
+	s = s.replace(/ワ/g, '和');
+	s = s.replace(/ヲ/g, '遠');
+
+	s = s.replace(/ン/g, '无');
+	
+	s = s.replace(/ガ/g, '加');
+	s = s.replace(/ギ/g, '機');
+	s = s.replace(/グ/g, '久');
+	s = s.replace(/ゲ/g, '計');
+	s = s.replace(/ゴ/g, '己');
+
+	s = s.replace(/ザ/g, '左');
+	s = s.replace(/ジ/g, '之');
+	s = s.replace(/ズ/g, '寸');
+	s = s.replace(/ゼ/g, '世');
+	s = s.replace(/ゾ/g, '曽');
+
+	s = s.replace(/ダ/g, '太');
+	s = s.replace(/ヂ/g, '知');
+	s = s.replace(/ヅ/g, '川');
+	s = s.replace(/デ/g, '天');
+	s = s.replace(/ド/g, '止');
+
+	s = s.replace(/バ/g, '波');
+	s = s.replace(/ビ/g, '比');
+	s = s.replace(/ブ/g, '不');
+	s = s.replace(/ｂｒ/g, '部');
+	s = s.replace(/ボ/g, '保');
+	
+	s = s.replace(/パ/g, '波');
+	s = s.replace(/ピ/g, '比');
+	s = s.replace(/プ/g, '不');
+	s = s.replace(/ペ/g, '部');
+	s = s.replace(/ポ/g, '保');
+	
+	return s;
 }
